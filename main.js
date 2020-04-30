f4700bdde703b406fb3a1484d8d9df06bc5a3c61
yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2020-04-30 17:53:38 +0900

    2020/04/30 17:30

--- main.js
+++ main.js
@@ -11,42 +11,108 @@ http
 const Discord = require("discord.js");
 const Client = new Discord.Client();
 
+//::実行:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 //##bot#######################################################################################################################
-const Bot = new Object();
-
-//::グローバルパラメータ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-Bot.parameter = {
-  adminName: "yaakiyu",
-  userIDs: {
-    yaakiyu: "693025129806037003",
-    uni: "687599949093011495"
+const Bot = new (class {
+  //::グローバルパラメータ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  constructor() {
+    this.parameter = {
+      adminName: "yaakiyu",
+      userIDs: {
+        yaakiyu: "693025129806037003",
+        uni: "687599949093011495"
+      },
+      saverConfigID: "703456600005804062"
+    };
+    this.database = {
+      system: null,
+      user: null,
+      variable: null
+    };
+    this.functions = new (class {
+      getAdminID() {
+        return Bot.parameter.userIDs[Bot.parameter.adminName];
+      }
+      getDataID(type) {
+        return new Promise((resolve, reject) => {
+          fetchMessages(Bot.parameter.saverConfigID, { limit: 1 }).then(
+            messages => {
+              resolve(JSON.parse(messages.first().content).databaseIDs[type]);
+            }
+          );
+        });
+      }
+    })();
   }
-};
-Bot.functions = new Object();
-Bot.functions.getAdminID = () => {
-  return Bot.parameter.userIDs[Bot.parameter.adminName];
-};
+  readDatabase() {
+    let setDataFunctions = [];
+    for (let key of Object.keys(this.database)) {
+      let setData = resolve => {
+        this.functions.getDataID(key).then(({ channel, message }) => {
+          fetchMessages(channel, message).then(messages => {
+            if (messages.content != void 0) {
+              this.database[key] = JSON.parse(messages.content);
+            } else {
+              this.database[key] = JSON.parse(messages.first().content);
+            }
+            resolve();
+          });
+        });
+      };
+      setDataFunctions.push(setData);
+    }
+    let setDataPromise = setDataFunctions.map(func => new Promise(func));
+    return Promise.all(setDataPromise);
+  }
+  Preparation() {
+    return this.readDatabase();
+  }
+  //::実行:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+  Run() {
+    Client.user.setActivity("ヘルプはdb:help");
+    jishoBot.Preparation();
+    Client.on("message", message => {
+      if (!message.author.bot) {
+        jishoBot.Run(message);
+        notice(message);
+      }
+    });
+    //send(`703421092869570665`, `{"JSON":0000}`);
+  }
+})();
 //::起動:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 Client.on("ready", message => {
-  console.log("準備完了");
-  log(0, "**Bot is ready!**");
-  Bot.Run();
-});
-//::実行:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-Bot.Run = () => {
-  Client.user.setActivity("ヘルプはdb:help");
-  jishoBot.Preparation();
-  Client.on("message", message => {
-    jishoBot.Run(message);
+  Bot.Preparation().then(() => {
+    console.log("準備完了");
+    log(0, "**Bot is ready!**");
+    //getLastMessage("703133624567988244");
+    console.log(Bot.database);
+    Bot.Run();
   });
-};
+});
 //::各種機能:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 const jishoBot = new Object();
 jishoBot.parameter = new Object();
+//**ローカルパラメータ**********************************************************************************
 jishoBot.parameter = {
   prefix: /^db:|^[&＆]/i
 };
-//**ローカルパラメータ**********************************************************************************
+const notice = message => {
+  const prefix = jishoBot.parameter.prefix;
+  if (message.content.match(prefix, "s-news")) {
+    const ch_name = "db-お知らせ";
+    Client.channels.cache.forEach(channel => {
+      let command = message.content.replace(prefix, "");
+      if (command.startsWith("s-news")) {
+        if (channel.name === ch_name) {
+          channel.send(message.content);
+          return;
+        }
+      }
+    });
+  }
+};
+
 jishoBot.parameter.dictionary = [
   {
     word: "s-ヘルプ|s-help|s-へるぷ",
@@ -59,7 +125,7 @@ jishoBot.parameter.dictionary = [
   },
   {
     word: "discord|ディスコード",
-    mean: `Discord(ディスコード)とは様々な機種で動作する、ゲーマー向けの匿名チャットツールである。
+    mean: `[**Discord(ディスコード)**](https://discordapp.com/)とは様々な機種で動作する、ゲーマー向けの匿名チャットツールである。
 テキストチャット以外にも、ボイスチャット、画面の共有、ビデオ通話機能などがある。ただし、画面の共有は現在web版では使えない。
 Web版、Windows版、Mac版、Android版、iOS版、Linux盤などがある。([ダウンロードはこちらから](https://discordapp.com/download))`
   },
@@ -70,7 +136,7 @@ Web版、Windows版、Mac版、Android版、iOS版、Linux盤などがある。(
   },
   {
     word: "nitro",
-    mean: `nitro(ニトロ)は、ディスコードで使える有料サービスである。別サーバーのカスタム絵文字が使用できるなど、特典がついている。`
+    mean: `「nitro(ニトロ)」は、ディスコードで使える有料サービスである。別サーバーのカスタム絵文字が使用できるなど、特典がついている。`
   },
   {
     word: "辞書",
@@ -102,25 +168,27 @@ NSFWは、性的コンテンツへのアクセスを禁止している職場や�
     word: "TakuTukirou|月浪たく",
     mean: `「takutukirou」は、T-takuの開発した多機能Botである。グローバルチャットが使えるほか、ゲームなどの娯楽要素も存在する。公式サーバーあり。
 （以下は本人からの要望により招待リンクを掲載させていただく。）
-[導入する](https://discordapp.com/api/oauth2/authorize?client_id=476012428170362880&permissions=2147347828&scope=bot)`
+[導入はこちらから](https://discordapp.com/api/oauth2/authorize?client_id=476012428170362880&permissions=2147347828&scope=bot)`
   },
   {
     word: "sina-chan|思惟奈ちゃん",
     mean: `**本人からの許可を得ていないので現在この項目を見ることは出来ません。**`
   },
   {
-    word: "佐藤　真美|佐藤真美",
-    mean: `「佐藤　真美」はjun50により開発された多機能Botである。主にボイスチャットでの機能が使えるが、「一文字ずつに☆を入れる」などの面白い機能も使える。公式サーバーあり。`
+    word: "佐藤 真美|佐藤真美",
+    mean: `「佐藤 真美」はjun50により開発された多機能Botである。主にボイスチャットでの機能が使えるが、「一文字ずつに☆を入れる」などの面白い機能も使える。公式サーバーあり。
+[導入はこちらから](https://discordapp.com/api/oauth2/authorize?client_id=479506436142006282&permissions=2083912785&scope=bot)`
   },
   {
     word: "wikipedia",
-    mean: `「ウィキペディア」は、フリーなインターネット辞書である。`
+    mean: `「ウィキペディア（Wikipedia）」は、フリーなインターネット辞書である。`
   }
 ];
 
 jishoBot.parameter.embedColor = 0x42f5f5;
 jishoBot.parameter.embedFooter = "ご利用いただきありがとうございます。";
 jishoBot.parameter.inProductionMessage = "<この項目は只今執筆中です...>";
+
 jishoBot.parameter.about = () => {
   let embed = new Discord.MessageEmbed();
   return embed
@@ -152,7 +220,7 @@ jishoBot.parameter.elseMessage = word => {
     .setColor(0xa103fc)
     .setTitle("すみません...")
     .setDescription(`${word}が見つかりませんでした。 `)
-    .setFooter("目次：db:s目次\nヘルプ：db:help");
+    .setFooter("目次：db:s-目次\nヘルプ：db:s-help");
 };
 
 //**ローカル関数軍************************************************************************************
@@ -191,6 +259,8 @@ jishoBot.Run = message => {
     const author = message.author;
     const elseMessage = jishoBot.parameter.elseMessage;
     const dictionary = jishoBot.parameter.dictionary;
+    //channel.send(JSON.stringify(Bot.database, null, 4));
+    //editLast(Bot.functions.getDataID("variable"), `{"test":null}`);
     let search = content.replace(prefix, "");
     if (search == "about") {
       channel.send(jishoBot.parameter.about());
@@ -202,6 +272,7 @@ jishoBot.Run = message => {
       );
       return;
     }
+    //channel.send({content:"test",embed:{title:"test"}})
     for (let item of dictionary) {
       const embedColor = jishoBot.parameter.embedColor;
       const embedFooter = jishoBot.parameter.embedFooter;
@@ -237,24 +308,24 @@ jishoBot.Run = message => {
     }
     if (!search == "") {
       channel.send(elseMessage(search));
-      Client.users.cache
+      /* Client.users.cache
         .get(Bot.functions.getAdminID())
         .send(
           `存在しない言葉「${search}」が検索されました。 by ${author.username}`
-        );
+        );*/
       log(
         1,
         `**Searched**  Status:[Couldn't hit]]  Word:[${search}]  Server:[${guild}]  Channel:[${channel.toString()}]  User:[${
           author.username
         } (${nickname})]`
       );
-      log(2,`**Item addition request**\nWords not found in the dictionary "${search}" have been serched.\nIf it isn't an inappropriate word,please add it.`)
+      log(2, `"${search}"`);
     }
   }
 };
 function log(channelNamber) {
   const all_logChannelID = "703130633857400895"; //起動通知がされるチャンネルID
-  const logChannelIDs = ["703133557769502741","705013092618076201"];
+  const logChannelIDs = ["703133557769502741", "705013092618076201"];
 
   let content = "";
   let index = 0;
@@ -284,6 +355,25 @@ function send(channelid, content) {
       .forEach(channel => {
         channel.send(content).then(message => {
           resolve(message);
+        });
+      });
+  });
+}
+function editLast(channelID, content) {
+  return new Promise((resolve, reject) => {
+    fetchMessages("703421092869570665", { limit: 1 }).then(messages => {
+      messages.first().edit(content);
+      resolve();
+    });
+  });
+}
+function fetchMessages(channelID, fetch) {
+  return new Promise((resolve, reject) => {
+    Client.channels.cache
+      .filter(channel => channel.id == channelID)
+      .forEach(channel => {
+        channel.messages.fetch(fetch).then(messages => {
+          resolve(messages);
         });
       });
   });
